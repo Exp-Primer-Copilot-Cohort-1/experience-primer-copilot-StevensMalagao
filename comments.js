f1cbec605582bbca7500abeaf11027e27124f8d6
StevensMalagao <135569765+StevensMalagao@users.noreply.github.com>
2023-12-06 04:14:23 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,49 @@
+// Create web server application
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Create a GET route for the comments page
+app.get('/comments', (req, res) => {
+  res.send('This is the comments page!');
+});
+
+// Create a GET route for the comments page
+app.get('/comments', (req, res) => {
+  res.send('This is the comments page!');
+});
+
+// Create a GET route for the comments page
+app.get('/comments', (req, res) => {
+  res.send('This is the comments page!');
+});
+
+// Create a GET route for the comments page
+app.get('/comments', (req, res) => {
+  res.send('This is the comments page!');
+});
+
+// Create a GET route for the comments page
+app.get('/comments', (req, res) => {
+  res.send('This is the comments page!');
+});
+
+// Create a GET route for the comments page
+app.get('/comments', (req, res) => {
+  res.send('This is the comments page!');
+});
+
+// Create a GET route for the comments page
+app.get('/comments', (req, res) => {
+  res.send('This is the comments page!');
+});
+
+// Create a GET route for the comments page
+app.get('/comments', (req, res) => {
+  res.send('This is the comments page!');
+});
+
+// Create a GET route for the comments page
+app.get('/comments', (req, res) => {
+  res.send('This is the comments page!');
+});
